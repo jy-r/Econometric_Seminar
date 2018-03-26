5694e852f5588d904d206f6e9d9e2698ce722abe
Unknown <georgiev.jiri@gmail.com>
2018-03-26 13:27:25 +0200

    .

--- install_packages.r
+++ install_packages.r
@@ -21,7 +21,8 @@ required_packages <- c("ggplot2",
                        "glmmML",
                        "rgeos",
                        "rgdal",
-                       "RColorBrewer")
+                       "RColorBrewer",
+                       "mice")
 missing_packages <-
   required_packages[!(required_packages %in% installed.packages()[, "Package"])]
 if (length(missing_packages) > 0) {
