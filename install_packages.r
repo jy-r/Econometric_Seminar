e41a8c64d66ab1cb691147916caa98fc795e57f1
Unknown <georgiev.jiri@gmail.com>
2018-06-15 15:12:00 +0200

    update...

--- install_packages.r
+++ install_packages.r
@@ -24,7 +24,8 @@ required_packages <- c("ggplot2",
                        "RColorBrewer",
                        "mice",
                        "bife",
-                       "survival")
+                       "survival",
+                       "rbenchmark")
 missing_packages <-
   required_packages[!(required_packages %in% installed.packages()[, "Package"])]
 if (length(missing_packages) > 0) {
